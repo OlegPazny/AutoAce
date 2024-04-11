f568bc2d69d9e4c45c5e9ab4c97b58767f6d877b
OlegPazny <petr.jakimovets@gmail.com>
2024-04-11 18:13:02 +0300

    починил гитигнор

---
+++ assets/api/mail/mail_client_connect.php
@@ -0,0 +1,5 @@
+<?php
+    require_once __DIR__.'/mail/vendor/autoload.php';
+    $settings=require_once __DIR__.'/mail/mail_settings.php';
+    require_once __DIR__.'/mail/functions.php';
+?>
--- includes/header.php
+++ includes/header.php
@@ -6,6 +6,17 @@
     <title></title>
 </head>
 <body>
-    
+    <header class="header">
+    <div class="header__logo">
+        <h1>logo</h1>
+    </div>
+    <div class="header__search">
+        <p><a href="#">Поиск автосервиса</a></p>
+    </div>
+    <div class="header__contact-info">
+        <div class="phone-number">+7 (123) 456-7890</div>
+        <div class="account-icon">Иконка аккаунта</div>
+    </div>
+    </header>
 </body>
 </html>
