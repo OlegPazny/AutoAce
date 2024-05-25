48134f564362599c469d2e2e022f576a32243dff
OlegPazny <petr.jakimovets@gmail.com>
2024-05-25 16:13:40 +0300

    иконка логина

--- includes/header.php
+++ includes/header.php
@@ -2,7 +2,7 @@
 require_once "../assets/api/isAdmin.php";
 if (!isset($_SESSION['user'])) {
     $account_href = "../pages/signin.php";
-    $account_icon = "<svg xmlns='http://www.w3.org/2000/svg' width='1.5em' height='1.5em' viewBox='0 0 24 24'><path fill='#fff' d='M12 21v-2h7V5h-7V3h7q.825 0 1.413.588T21 5v14q0 .825-.587 1.413T19 21zm-2-4l-1.375-1.45l2.55-2.55H3v-2h8.175l-2.55-2.55L10 7l5 5z'/></svg>";
+    $account_icon = "<img src='../assets/images/login_icon.svg' style='width: 1.5em; height:1.5em'>";
     $header_list = "<li id='sign-in__link'>Войти</li>";
 } else if (isset($_SESSION['user']) && $isMechanic == false) {
     $account_href = "../pages/account.php";
