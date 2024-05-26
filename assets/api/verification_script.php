d53c78227b08bd03b1b2eec8fe8630d91019df34
OlegPazny <petr.jakimovets@gmail.com>
2024-05-26 20:14:41 +0300

    верификацияя переделал

--- assets/api/verification_script.php
+++ assets/api/verification_script.php
@@ -1,19 +1,23 @@
 <?php
     require_once "db_connect.php";
-
-    $hash=$_GET['hash'];
-
-    $users_data=mysqli_query($db, "SELECT * FROM `users` WHERE `isVerified`=0");
-    $users_data=mysqli_fetch_all($users_data);
-
+    
     $isVerified=false;
-    foreach($users_data as $user_data){
-        $toVerify=md5($user_data[1].$user_data[2].$user_data[3]);
-        if($hash===$toVerify){
-            $isVerified=true;
-            $verification=mysqli_query($db, "UPDATE `users` SET `isVerified`=1 WHERE `id`=$user_data[0]");
-        }else{
-            $isVerified=false;
+    if(isset($_GET['hash'])){
+        $hash=$_GET['hash'];
+
+        $users_data=mysqli_query($db, "SELECT * FROM `users` WHERE `isVerified`=0");
+        $users_data=mysqli_fetch_all($users_data);
+    
+
+        foreach($users_data as $user_data){
+            $toVerify=md5($user_data[1].$user_data[2].$user_data[3]);
+            if($hash===$toVerify){
+                $isVerified=true;
+                $verification=mysqli_query($db, "UPDATE `users` SET `isVerified`=1 WHERE `id`=$user_data[0]");
+            }else{
+                $isVerified=false;
+            }
         }
     }
+    
 ?>
