63d3af03a21bae08268cdf9d95150e90a1bb7e06
OlegPazny <petr.jakimovets@gmail.com>
2024-06-01 19:11:37 +0300

    404

---
+++ pages/404.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="../assets/images/favicon.svg" />
+    <title>Страница не найдена!</title>
+    <link rel="stylesheet" type="text/css" href="../assets/css/main.css">
+</head>
+<body>
+    <section class="section-404">
+        <div class="block-404">
+            <div class="block-404__head">
+                <p class="block-404__head__number">4</p>
+                <div class="block-404__head__icon-block">
+                    <div class="block-404__head__icon"><img src="../assets/images/404_icon.svg"></div>
+                    <div class="block-404__head__shadow"><img src="../assets/images/404_shadow.svg"></div>
+                </div>
+                <p class="block-404__head__number">4</p>
+            </div>
+            <div class="block-404__text">Такая страница не найдена</div>
+            <a href='../index.php'>
+                <div class="signup__submit-block button login-btn-block" style="width: auto">
+                    <input type="button" class="login-btn button__content" value="На главную">
+                </div>
+            </a>
+        </div>
+    </section>
+</body>
+</html>
